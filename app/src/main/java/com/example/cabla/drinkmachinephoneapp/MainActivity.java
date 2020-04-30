5ffe95231bbccf750172ecad9450181841ab54c5
ChrisBlanks <cablanks@hotmail.com>
2020-04-29 20:32:11 -0400

    Update project for Android SDK 29

--- app/src/main/java/com/example/cabla/drinkmachinephoneapp/MainActivity.java
+++ app/src/main/java/com/example/cabla/drinkmachinephoneapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.cabla.drinkmachinephoneapp;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
--- app/src/main/java/com/example/cabla/drinkmachinephoneapp/SalesFragment.java
+++ app/src/main/java/com/example/cabla/drinkmachinephoneapp/SalesFragment.java
@@ -1,7 +1,7 @@
 package com.example.cabla.drinkmachinephoneapp;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
